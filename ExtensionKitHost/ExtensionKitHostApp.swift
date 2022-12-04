097244309969ca9a7e32f5ada257862d1a4029be
Wouter01 <wouterhennen@gmail.com>
2022-12-04 03:33:51 +0100

    Initial Commit

---
+++ ExtensionKitHost/ExtensionKitHostApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExtensionKitHostApp.swift
+//  ExtensionKitHost
+//
+//  Created by Wouter Hennen on 04/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExtensionKitHostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
